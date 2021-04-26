5e7542793772b66138468e0f87e0c34cfa5dd673
obiPlabon <obiplabon@gmail.com>
2021-04-26 16:32:41 +0600

    Guest review condition applied

--- templates/single-reviews.php
+++ templates/single-reviews.php
@@ -20,10 +20,10 @@ use function wpWax\Directorist\Review\get_criteria_names;
 use function wpWax\Directorist\Review\is_criteria_enabled;
 use function wpWax\Directorist\Review\get_media_uploader_markup;
 
-$listing         = Directorist_Listing::instance();
-$review_rating   = Review_Data::get_rating( get_the_ID() );
-$review_count    = Review_Data::get_review_count( get_the_ID() );
-$criteria_rating = Review_Data::get_criteria_rating( get_the_ID() );
+$listing           = Directorist_Listing::instance();
+$review_rating     = Review_Data::get_rating( get_the_ID() );
+$review_count      = Review_Data::get_review_count( get_the_ID() );
+$criteria_rating   = Review_Data::get_criteria_rating( get_the_ID() );
 ?>
 
 <div class="directorist-review-container">
@@ -89,96 +89,98 @@ $criteria_rating = Review_Data::get_criteria_rating( get_the_ID() );
 	</div><!-- ends: .directorist-review-content -->
 
 	<?php
-	$commenter = wp_get_current_commenter();
-	$req       = get_option( 'require_name_email' );
-	$html_req  = ( $req ? " required='required'" : '' );
+	if ( is_user_logged_in() || get_directorist_option( 'guest_review', 0 ) ) {
+		$commenter = wp_get_current_commenter();
+		$req       = get_option( 'require_name_email' );
+		$html_req  = ( $req ? " required='required'" : '' );
 
-	$fields = array(
-		'author' => sprintf(
-			'<div class="directorist-form-group form-group-author">%s %s</div>',
-			sprintf(
-				'<label for="author">%s%s</label>',
-				esc_html__( 'Name', 'directorist' ),
-				( $req ? ' <span class="required">*</span>' : '' )
+		$fields = array(
+			'author' => sprintf(
+				'<div class="directorist-form-group form-group-author">%s %s</div>',
+				sprintf(
+					'<label for="author">%s%s</label>',
+					esc_html__( 'Name', 'directorist' ),
+					( $req ? ' <span class="required">*</span>' : '' )
+				),
+				sprintf(
+					'<input id="author" class="directorist-form-element" placeholder="%s" name="author" type="text" value="%s" size="30" maxlength="245"%s />',
+					esc_attr__( 'Enter your name', 'directorist' ),
+					esc_attr( $commenter['comment_author'] ),
+					$html_req
+				)
 			),
-			sprintf(
-				'<input id="author" class="directorist-form-element" placeholder="%s" name="author" type="text" value="%s" size="30" maxlength="245"%s />',
-				esc_attr__( 'Enter your name', 'directorist' ),
-				esc_attr( $commenter['comment_author'] ),
-				$html_req
-			)
-		),
-		'email'  => sprintf(
-			'<div class="directorist-form-group form-group-email">%s %s</div>',
-			sprintf(
-				'<label for="email">%s%s</label>',
-				esc_html__( 'Email', 'directorist' ),
-				( $req ? ' <span class="required">*</span>' : '' )
+			'email'  => sprintf(
+				'<div class="directorist-form-group form-group-email">%s %s</div>',
+				sprintf(
+					'<label for="email">%s%s</label>',
+					esc_html__( 'Email', 'directorist' ),
+					( $req ? ' <span class="required">*</span>' : '' )
+				),
+				sprintf(
+					'<input id="email" class="directorist-form-element" placeholder="%s" name="email" type="email" value="%s" size="30" maxlength="100" aria-describedby="email-notes"%s />',
+					esc_attr__( 'Enter your email', 'directorist' ),
+					esc_attr( $commenter['comment_author_email'] ),
+					$html_req
+				)
 			),
-			sprintf(
-				'<input id="email" class="directorist-form-element" placeholder="%s" name="email" type="email" value="%s" size="30" maxlength="100" aria-describedby="email-notes"%s />',
-				esc_attr__( 'Enter your email', 'directorist' ),
-				esc_attr( $commenter['comment_author_email'] ),
-				$html_req
-			)
-		),
-		'url'    => sprintf(
-			'<div class="directorist-form-group form-group-url">%s %s</div>',
-			sprintf(
-				'<label for="url">%s</label>',
-				esc_html__( 'Website', 'directorist' ),
+			'url'    => sprintf(
+				'<div class="directorist-form-group form-group-url">%s %s</div>',
+				sprintf(
+					'<label for="url">%s</label>',
+					esc_html__( 'Website', 'directorist' ),
+				),
+				sprintf(
+					'<input id="url" class="directorist-form-element" placeholder="%s" name="url" type="url" value="%s" size="30" maxlength="200" />',
+					esc_attr__( 'Enter your website', 'directorist' ),
+					esc_attr( $commenter['comment_author_url'] )
+				)
 			),
+		);
+
+		$comment_field = sprintf(
+			'<div class="directorist-form-group form-group-comment">%s %s</div>',
 			sprintf(
-				'<input id="url" class="directorist-form-element" placeholder="%s" name="url" type="url" value="%s" size="30" maxlength="200" />',
-				esc_attr__( 'Enter your website', 'directorist' ),
-				esc_attr( $commenter['comment_author_url'] )
+				'<label for="comment">%s</label>',
+				_x( 'Comment', 'noun', 'directorist' )
+			),
+			sprintf( '<textarea id="comment" class="directorist-form-element" placeholder="%s" name="comment" cols="30" rows="10" maxlength="65525" required="required"></textarea>',
+				esc_attr__( 'Share your experience and help others make better choices', 'directorist' )
 			)
-		),
-	);
+		);
 
-	$comment_field = sprintf(
-		'<div class="directorist-form-group form-group-comment">%s %s</div>',
-		sprintf(
-			'<label for="comment">%s</label>',
-			_x( 'Comment', 'noun', 'directorist' )
-		),
-		sprintf( '<textarea id="comment" class="directorist-form-element" placeholder="%s" name="comment" cols="30" rows="10" maxlength="65525" required="required"></textarea>',
-			esc_attr__( 'Share your experience and help others make better choices', 'directorist' )
-		)
-	);
+		$criteria_markup = '<div class="directorist-review-criteria">%s</div>' . "\n";
 
-	$criteria_markup = '<div class="directorist-review-criteria">%s</div>' . "\n";
+		if ( is_criteria_enabled() ) {
+			$criteria_items_markup = '';
+			foreach ( get_criteria_names() as $criteria_key => $criteria_name ) {
+				$criteria_items_markup .= get_rating_markup( $criteria_name, $criteria_key ) . "\n";
+			}
 
-	if ( is_criteria_enabled() ) {
-		$criteria_items_markup = '';
-		foreach ( get_criteria_names() as $criteria_key => $criteria_name ) {
-			$criteria_items_markup .= get_rating_markup( $criteria_name, $criteria_key ) . "\n";
+			$criteria_markup = sprintf( $criteria_markup, $criteria_items_markup );
+
+			unset( $criteria_items_markup );
+		} else {
+			$criteria_markup = sprintf( $criteria_markup, get_rating_markup( __( 'Rating', 'directorist' ) ) );
 		}
 
-		$criteria_markup = sprintf( $criteria_markup, $criteria_items_markup );
+		$comment_field = $criteria_markup . "\n" . $comment_field . "\n" . get_media_uploader_markup();
 
-		unset( $criteria_items_markup );
-	} else {
-		$criteria_markup = sprintf( $criteria_markup, get_rating_markup( __( 'Rating', 'directorist' ) ) );
+		$args = array(
+			'fields'             => $fields,
+			'comment_field'      => $comment_field,
+			'class_container'    => 'directorist-review-submit',
+			'title_reply'        => __( 'Leave a Review', 'directorist' ),
+			'title_reply_before' => '<div class="directorist-review-submit__header"><h3 id="reply-title">',
+			'title_reply_after'  => '</h3></div>',
+			'class_form'         => 'comment-form directorist-review-submit__form',
+			'class_submit'       => 'directorist-btn directorist-btn-primary',
+			'label_submit'       => __( 'Submit your review', 'directorist' ),
+			'format'             => 'html5',
+			'submit_field'       => '<div class="directorist-form-group">%1$s %2$s</div>',
+			'submit_button'      => '<button name="%1$s" type="submit" id="%2$s" class="%3$s" value="%4$s">%4$s</button>',
+		);
+
+		comment_form( $args );
 	}
-
-	$comment_field = $criteria_markup . "\n" . $comment_field . "\n" . get_media_uploader_markup();
-
-	$args = array(
-		'fields'             => $fields,
-		'comment_field'      => $comment_field,
-		'class_container'    => 'directorist-review-submit',
-		'title_reply'        => __( 'Leave a Review', 'directorist' ),
-		'title_reply_before' => '<div class="directorist-review-submit__header"><h3 id="reply-title">',
-		'title_reply_after'  => '</h3></div>',
-		'class_form'         => 'comment-form directorist-review-submit__form',
-		'class_submit'       => 'directorist-btn directorist-btn-primary',
-		'label_submit'       => __( 'Submit your review', 'directorist' ),
-		'format'             => 'html5',
-		'submit_field'       => '<div class="directorist-form-group">%1$s %2$s</div>',
-		'submit_button'      => '<button name="%1$s" type="submit" id="%2$s" class="%3$s" value="%4$s">%4$s</button>',
-	);
-
-	comment_form( $args );
 	?>
 </div>
