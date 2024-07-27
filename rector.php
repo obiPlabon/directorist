13e54af9d3146b0ebb6a5d3cdada4e066225b6dd
obiPlabon <obi.plabon@gmail.com>
2024-07-27 17:02:58 +0600

    Rector setup done

--- rector.php
+++ rector.php
@@ -1,37 +1,23 @@
 <?php
+
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
-use Rector\Core\ValueObject\PhpVersion;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    // $rectorConfig->paths([
-    //     __DIR__ . '/src'
-    // ]);
-
-    // register a single rule
-    // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
-
-    // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_74
-    //    ]);
-
-	$rectorConfig->paths([__DIR__ . '/includes/gutenberg']);
-
-	$rectorConfig->phpVersion(PhpVersion::PHP_70);
-
-	$rectorConfig->sets([
-        SetList::CODE_QUALITY
-    ]);
-
-    // register single rule
-    // $rectorConfig->rule(TypedPropertyRector::class);
-
-	$rectorConfig->skip([
-		CallableThisArrayToAnonymousFunctionRector::class
-	]);
-};
+return RectorConfig::configure()
+	->withRootFiles()
+    ->withPaths([
+		__DIR__ . '/includes/classes/class-add-listing.php',
+		// __DIR__ . '/includes/model/Listings.php',
+        // __DIR__ . '/includes/fields',
+        // __DIR__ . '/views',
+		// __DIR__ . '/templates',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets(php70: true)
+	// ->withSets([
+	// 	SetList::PHP_70
+	// ])
+	->withFileExtensions(['php'])
+    ->withTypeCoverageLevel(10);
